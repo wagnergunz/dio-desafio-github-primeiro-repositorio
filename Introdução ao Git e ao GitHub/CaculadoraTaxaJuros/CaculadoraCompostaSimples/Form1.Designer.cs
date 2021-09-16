d394ca2d4afa19df8026c316387605eae2f7fb17
wagnergunz <galaxdia@gmail.com>
2021-09-16 09:04:04 -0300

    Adicionado calculador composta simples em c# Form, no desafio

---
+++ Introdução ao Git e ao GitHub/CaculadoraTaxaJuros/CaculadoraCompostaSimples/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace CaculadoraCompostaSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Principal = new System.Windows.Forms.RichTextBox();
+            this.Taxa = new System.Windows.Forms.RichTextBox();
+            this.Periodo = new System.Windows.Forms.RichTextBox();
+            this.Montante = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(233, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Principal
+            // 
+            this.Principal.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Principal.Location = new System.Drawing.Point(233, 32);
+            this.Principal.Name = "Principal";
+            this.Principal.Size = new System.Drawing.Size(161, 41);
+            this.Principal.TabIndex = 2;
+            this.Principal.Text = "";
+            this.Principal.TextChanged += new System.EventHandler(this.Principal_TextChanged);
+            // 
+            // Taxa
+            // 
+            this.Taxa.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Taxa.Location = new System.Drawing.Point(233, 94);
+            this.Taxa.Name = "Taxa";
+            this.Taxa.Size = new System.Drawing.Size(161, 41);
+            this.Taxa.TabIndex = 3;
+            this.Taxa.Text = "";
+            this.Taxa.TextChanged += new System.EventHandler(this.Taxa_TextChanged);
+            // 
+            // Periodo
+            // 
+            this.Periodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Periodo.Location = new System.Drawing.Point(233, 158);
+            this.Periodo.Name = "Periodo";
+            this.Periodo.Size = new System.Drawing.Size(161, 41);
+            this.Periodo.TabIndex = 4;
+            this.Periodo.Text = "";
+            this.Periodo.TextChanged += new System.EventHandler(this.Periodo_TextChanged);
+            // 
+            // Montante
+            // 
+            this.Montante.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Montante.Location = new System.Drawing.Point(233, 222);
+            this.Montante.Name = "Montante";
+            this.Montante.Size = new System.Drawing.Size(161, 41);
+            this.Montante.TabIndex = 5;
+            this.Montante.Text = "";
+            this.Montante.TextChanged += new System.EventHandler(this.Montante_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.Location = new System.Drawing.Point(12, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 35);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Valor da Parcela";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox2.Location = new System.Drawing.Point(12, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 38);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Taxa de Juros";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox3.Location = new System.Drawing.Point(12, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 35);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Dias em Atraso";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox4.Location = new System.Drawing.Point(12, 222);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 35);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Valor a Pagar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(420, 416);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Montante);
+            this.Controls.Add(this.Periodo);
+            this.Controls.Add(this.Taxa);
+            this.Controls.Add(this.Principal);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calculadora.ByWagnerP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox Principal;
+        private System.Windows.Forms.RichTextBox Taxa;
+        private System.Windows.Forms.RichTextBox Periodo;
+        private System.Windows.Forms.RichTextBox Montante;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Introdução ao Git e ao GitHub/CaculadoraTaxaJuros/CaculadoraCompostaSimples/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaculadoraCompostaSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text != "")
+            { 
+            double M;
+            double P = Convert.ToDouble(Principal.Text);
+            double i = Convert.ToDouble(Taxa.Text) / 100;
+            double n = Convert.ToDouble(Periodo.Text);
+            M = P * (1 + (i * (n / 30)));
+            Montante.Text = M.ToString("0.00");
+
+            }else
+            {
+                MessageBox.Show("Digite os campos para ter o resultado! ");
+            }
+        }
+
+        private void Principal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Taxa_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Periodo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Montante_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
